61cd52965f45b3796e00d5978a9f4c0d2d5a0e53
loftyshaky <loftyshaky@gmail.com>
2018-11-21 03:22:39 +1000

    Refactor folder opening / showing (in file explorer) + add error handling

--- src/js/show_or_open_folder.js
+++ src/js/show_or_open_folder.js
@@ -2,18 +2,32 @@ import { shell } from 'electron';
 
 import * as shared from 'js/shared';
 
-export const show_folder = () => {
-    const shown = shell.showItemInFolder(shared.ob.chosen_folder_path);
+const show_or_open_folder = (fun, err1, err2) => {
+    try {
+        if (shared.ob.chosen_folder_path !== '') {
+            const folder_opened_in_explorer = shell[fun](shared.ob.chosen_folder_path);
 
-    if (!shown) {
-        err(er_obj('Cant show folder'), 140);
+            if (!folder_opened_in_explorer) {
+                err1();
+            }
+
+        } else {
+            err2();
+        }
+
+    } catch (er) {
+        err(er, 144);
     }
 };
 
-export const open_folder = () => {
-    const opened = shell.openItem(shared.ob.chosen_folder_path);
+export const show_folder = () => {
+    const er_obj_instance = er_obj('Cant show folder');
 
-    if (!opened) {
-        err(er_obj('Cant open folder'), 141);
-    }
+    show_or_open_folder('showItemInFolder', err.bind(null, er_obj_instance, 140), err.bind(null, er_obj_instance, 141, 'cant_show_folder'));
+};
+
+export const open_folder = () => {
+    const er_obj_instance = er_obj('Cant open folder');
+
+    show_or_open_folder('openItem', err.bind(null, er_obj_instance, 142), err.bind(null, er_obj_instance, 143, 'cant_open_folder'));
 };
