1e289819b583780011b13f789431d4fe762a3f9e
loftyshaky <loftyshaky@gmail.com>
2018-12-28 00:23:18 +1000

    Check for updates only in production

--- main.js
+++ main.js
@@ -132,13 +132,15 @@ function create_window() {
     });
     //< context menu
 
-    //> auto update
-    autoUpdater.checkForUpdates();
+    if (!global.dev) {
+        //> auto update
+        autoUpdater.checkForUpdates();
 
-    autoUpdater.on('update-downloaded', () => {
-        main_window.webContents.send('update_downloaded');
-    });
-    //< auto update
+        autoUpdater.on('update-downloaded', () => {
+            main_window.webContents.send('update_downloaded');
+        });
+        //< auto update
+    }
 }
 
 app.on('ready', create_window); // this method will be called when Electron has finished initialization and is ready to create browser windows. some APIs can only be used after this event occurs.
