e06d284220a7524d6344f096ebeca2db98aa2169
loftyshaky <loftyshaky@gmail.com>
2019-01-07 04:19:09 +1000

    Fix analytics privacy popup position

--- src/js/analytics_privacy.js
+++ src/js/analytics_privacy.js
@@ -3,6 +3,7 @@
 import { action, configure } from 'mobx';
 import Store from 'electron-store';
 
+import x from 'x';
 import { inputs_data } from 'js/inputs_data';
 import * as shared from 'js/shared';
 import * as analytics from 'js/analytics';
@@ -11,8 +12,10 @@ import * as toggle_popup from 'js/toggle_popup';
 configure({ enforceActions: 'observed' });
 const store = new Store();
 
-export const center_analytics_privacy = () => {
+export const center_analytics_privacy = async () => {
     try {
+        await x.delay(0); // without this offsetHeight will be incorrect
+
         const analytics_privacy = s('.analytics_privacy');
 
         analytics_privacy.style.marginTop = `-${analytics_privacy.offsetHeight / 2}px`;
