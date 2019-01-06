1c560a96d84ca894d3808b84399c116e25d251fd
loftyshaky <loftyshaky@gmail.com>
2019-01-06 20:31:49 +1000

    Add privacy policy link to Analytics_privacy popup

--- src/js/components/Analytics_privacy.jsx
+++ src/js/components/Analytics_privacy.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 
+import * as analytics from 'js/analytics';
 import * as analytics_privacy from 'js/analytics_privacy';
 import * as toggle_popup from 'js/toggle_popup';
 
@@ -27,14 +28,26 @@ export class Analytics_privacy extends React.Component {
                     className="analytics_privacy_message"
                     data-text="analytics_privacy_message_text"
                 />
-                <Btn
-                    name="allow_analytics"
-                    on_click={() => analytics_privacy.allow_or_disallow_analytics(true, 'allow_analytics')}
-                />
-                <Btn
-                    name="disallow_analytics"
-                    on_click={() => analytics_privacy.allow_or_disallow_analytics(false, 'disallow_analytics')}
-                />
+                <div className="analytics_privacy_btns_and_link">
+                    <Btn
+                        name="allow_analytics"
+                        on_click={() => analytics_privacy.allow_or_disallow_analytics(true, 'allow_analytics')}
+                    />
+                    <Btn
+                        name="disallow_analytics"
+                        on_click={() => analytics_privacy.allow_or_disallow_analytics(false, 'disallow_analytics')}
+                    />
+                    <a
+                        className="privacy_policy_link"
+                        href="https://bit.ly/ctc-privacy-policy"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        data-text="privacy_policy_link_text"
+                        onClick={analytics.send_event.bind(null, 'links', 'clicked-clicked-analytics_privacy-privacy_policy_link')}
+                    >
+                        content
+                    </a>
+                </div>
             </Tr>
         );
     }
