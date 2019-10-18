607e0d9b3434a042d022aa1c567dcf2cbff1c198
loftyshaky <loftyshaky@gmail.com>
2019-10-19 09:21:38 +1000

    Add padding to the left and right sides of fieldset when they're wrapped

--- src/js/components/Fieldset.jsx
+++ src/js/components/Fieldset.jsx
@@ -27,25 +27,25 @@ export const Fieldset = observer(props => {
         <div className={x.cls(['fieldset_w', `${name}_fieldset_w`])}>
             <Tr
                 attr={{
-                    className: 'legend',
-                }}
-                tag="div"
-                name="legend"
-                state={els_state.com.fieldset_protecting_screen_is_visible}
-            >
-                {x.msg(`${name}_legend_text`)}
-                <div className="legend_line" />
-            </Tr>
-            <div className="cover" />
-            <Tr
-                attr={{
                     className: `${name}_fieldset`,
                 }}
                 tag="fieldset"
                 name="fieldset"
                 state={els_state.com.fieldset_protecting_screen_is_visible}
             >
-                <div>
+                <Tr
+                    attr={{
+                        className: 'legend',
+                    }}
+                    tag="div"
+                    name="legend"
+                    state={els_state.com.fieldset_protecting_screen_is_visible}
+                >
+                    {x.msg(`${name}_legend_text`)}
+                    <div className="legend_line" />
+                </Tr>
+                <div className="cover" />
+                <div className="fieldset_content_w">
                     <div className="fieldset_content">
                         <div className="history_fieldset_protecting_screen_w">
                             <History_fieldset_protecting_screen name={name} />
